a7d738dd72b6408bd5b2a7d5bd9046481cfc5335
Nithish-github <hiiamnithish@gmail.com>
2024-07-21 22:47:19 +0530

    July-2024-21

--- linked_list/2. Add Two Numbers.py
+++ linked_list/2. Add Two Numbers.py
@@ -4,25 +4,34 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        '''
+        Linked List Problem
+        '''
+
+        dummy  = ListNode()
+        cur = dummy 
+        carry = 0
+
+        while l1 or l2 or carry:
+
+            value1 = l1.val if l1 else 0
+            value2 = l2.val if l2 else 0
+
+            value = value1 + value2 + carry
+
+            carry = value // 10
+            value_to_append = value % 10
+
+            #update the current value to the linked list
+            cur.next = ListNode(value_to_append)
+
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+
+        return dummy.next
+
         
-        '''
-        Linked list Problem
-        '''
-        head = ListNode()
-        current  = head
-
-        while list1 and list2:
-            if list1.val <list2.val:
-                current.next = list1
-                list1 = list1.next
-
-            else:
-                current.next = list2
-                list2 = list2.next
-
-            current  = current.next
-
-        current.next = list1 or list2
-
-        return head.next
